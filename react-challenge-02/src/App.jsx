fad717064a6eea7e8ce0c11026076a82be3a8ab4
freaklucas <lucas.oliveira.freak@gmail.com>
2023-03-06 18:36:50 -0300

    feat:desafio concluido

---
+++ react-challenge-02/src/App.jsx
@@ -0,0 +1,86 @@
+import { login } from './utils';
+import './index.css';
+import { useState } from 'react';
+
+// Instruções:
+// * Você tem um formulário de login INCOMPLETO
+// * Não é permitido adicionar novos elementos HTML
+// * Não é permitido usar refs
+//
+// Tarefas:
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// (OK) todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// (OK) todo - Desabilite o botão de Login equanto você está executando o login.
+// (OK) todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isRequesting, setIsRequesting] = useState(false);
+  
+  const handleSubmit = () => {
+    setError(null);
+    setIsRequesting(true);
+    
+    let values = {email: email, password: password};
+    login(values).then(() => {
+      alert('Login efetuado com sucesso!');
+    }).catch((error) => {      
+      console.log(error);
+      setError(error);
+    }).finally(() => {
+      setIsRequesting(false);
+    });
+
+  }
+
+  const handleEmail = (event) => {
+    const {value} = event.target;
+    setEmail(value);
+  }
+
+  const handlePassword = (event) => {
+    const {value} = event.target;
+    setPassword(value);
+  }
+
+  return (
+    <div className='wrapper'>
+      <div className='login-form'>
+        <h1>Login Form 🐞</h1>
+        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+        {error && <div className='errorMessage'>{error.message}</div>}
+        <div className='row'>
+          <label htmlFor={'email'}>Email</label>
+          <input 
+            value={email}
+            onChange={handleEmail}
+            id={'email'} 
+            type={'email'} 
+            autoComplete='off' 
+          />
+        </div>
+        <div className='row'>
+          <label htmlFor={'password'}>Password</label>
+          <input 
+            onChange={handlePassword}
+            value={password}
+            id={'password'} 
+            type={'password'} 
+          />
+        </div>
+
+        <div className='button'>
+          <button 
+            onClick={handleSubmit}
+            disabled={email === '' || password.length < 6 || isRequesting  }
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
